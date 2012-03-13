fcc1dcf4778030bbb9d347d6b134b51e76e53656
seymor <zse.vad@gmail.com>
2012-03-13 12:14:25 +0100

    * modified gitignore file

--- gen/org/droidshell/R.java
+++ gen/org/droidshell/R.java
@@ -11,7 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int biz=0x7f020000;
+        public static final int explosion=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int splash=0x7f020003;
+        public static final int walk=0x7f020004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
